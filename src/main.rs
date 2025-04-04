3cf4377bc5d38a19a9550e8017c1c284c7333589
phantomboy0 <amir@amiruam.ir>
2025-04-05 00:18:28 +0330

    init

---
+++ src/main.rs
@@ -0,0 +1,261 @@
+mod repository;
+use repository::database::Db;
+use clap::{value_parser, Arg, ArgAction, Command};
+use colored::Colorize;
+use tabled::{ Table};
+use tabled::settings::{Color, Style};
+use tabled::settings::object::Rows;
+use tabled::settings::themes::Colorization;
+
+fn main() {
+    let matches = Command::new("dolist").
+        version("v1.0.0").
+        author("phantomboy0").
+        about("CLI to do list").
+        help_template("Dolist {version} By {author} \n{about} \n\n{usage}\n\n{all-args}").
+        //$ dolist new "Buy food" "buy orange and bleach for home"
+        subcommand(Command::new("new").
+            about("Create a new to-do item").
+
+                arg(Arg::new("name").
+                help("Title of the to-do item").
+                required(true).
+                index(1)).
+
+                arg(Arg::new("description").
+                help("Description of this item").
+                index(2))).
+        
+        //$ dolist list 1 -l 15 -a
+        subcommand(Command::new("list").
+            about("Shows list of todo items").
+
+            arg(Arg::new("page").
+                help("Page number of the list").
+                value_parser(value_parser!(u32)).
+                default_value("1").
+                index(1)).
+
+            arg(Arg::new("limit").
+                long("limit").
+                short('l').
+                help("Limit number of items in list per page").
+                value_parser(value_parser!(u32)).
+                default_value("10")).
+
+            arg(Arg::new("all").
+                long("all").
+                short('a').
+                help("Shows the entire items in the list").
+                action(ArgAction::SetTrue))).
+
+        //$ dolist done 1
+        subcommand(Command::new("done").
+            about("Set a Item's Status as Done").
+
+            arg(Arg::new("ID").
+                help("ID of the target item").
+                value_parser(value_parser!(u32)).
+                required(true).
+                index(1))).
+        
+        //$ dolist notdone 1
+        subcommand(Command::new("notdone").
+            about("Set a Item's Status as NotDone").
+
+            arg(Arg::new("ID").
+                help("ID of the target item").
+                value_parser(value_parser!(u32)).
+                required(true).
+                index(1))).
+        
+        //$ dolist delete 1
+        subcommand(Command::new("delete").
+            about("Delete a Item").
+
+            arg(Arg::new("ID").
+                help("ID of the target item").
+                value_parser(value_parser!(u32)).
+                required(true).
+                index(1))).
+        
+        //$ dolist edit 1 -n "Chicken jerky" -d "buy some chicken jerky from movies"
+        subcommand(Command::new("edit").
+            about("Edit a Item").
+
+            arg(Arg::new("ID").
+                help("ID of the target item").
+                value_parser(value_parser!(u32)).
+                required(true).
+                index(1)).
+        
+            arg(Arg::new("name").
+                help("New name for the item").
+                long("name").
+                short('n')).
+
+            arg(Arg::new("description").
+                help("New description for the item").
+                long("description").
+                short('d'))).
+        
+
+        get_matches();
+
+
+    match matches.subcommand() {
+        Some(("new", sub_matches)) => {
+            let name = sub_matches.get_one::<String>("name").unwrap();
+            let description = match sub_matches.get_one::<String>("description") {
+                Some(desc) => desc,
+                None => ""
+            };
+            create_new_item(name, description);
+        }
+
+        Some(("list", sub_matches)) => {
+            let page = sub_matches.get_one::<u32>("page").unwrap();
+            let limit = sub_matches.get_one::<u32>("limit").unwrap();
+            let all  = sub_matches.get_flag("all");
+            show_list_items(page,limit,all);
+        }
+
+        Some(("done", sub_matches)) => {
+            let id = sub_matches.get_one::<u32>("ID").unwrap();
+            set_item_status(id,&true);
+        }
+
+        Some(("notdone", sub_matches)) => {
+            let id = sub_matches.get_one::<u32>("ID").unwrap();
+            set_item_status(id,&false);
+        }
+
+        Some(("delete", sub_matches)) => {
+            let id = sub_matches.get_one::<u32>("ID").unwrap();
+            delete_item(id);
+        }
+        
+        Some(("edit", sub_matches)) => {
+            let id = sub_matches.get_one::<u32>("ID").unwrap();
+            let name = match sub_matches.get_one::<String>("name") {
+                Some(name) => name,
+                None => ""
+            };
+            let description = match sub_matches.get_one::<String>("description") {
+                Some(desc) => desc,
+                None => ""
+            };
+            edit_item(id,name,description);
+        }
+
+        _ => {
+            println!("No command specified.");
+            println!("Run 'dolist --help' for usage information.");
+        }
+    }
+}
+
+fn create_new_item(name: &str, description: &str) {
+    let app = match Db::new() {
+        Ok(db) => db,
+        Err(_) =>  panic!("Can't connect to the database.")
+    };
+
+    app.add_item(name, description).expect("Couldn't add the item");
+    println!("\n{}", "Added the new item to todo list".green());
+}
+
+fn show_list_items(page: &u32, limit: &u32, show_all: bool) {
+
+    if page <= &0 {
+        println!("The page number should be a positive number");
+        return;
+    }
+
+    if limit <= &0 {
+        println!("The limit number should be a positive number");
+        return;
+    }
+
+    let app = match Db::new() {
+        Ok(db) => db,
+        Err(_) =>  panic!("Can't connect to the database.")
+    };
+
+    let items = if show_all { app.show_all_items() } else { app.show_items(page,limit) };
+
+    if items.is_err() {
+        panic!("Can't get the items from database.");
+    }
+
+    let items =  items.unwrap();
+
+    if items.is_empty() {
+        if *page == 1 {
+            println!("No todo items to show.");
+            return;
+        }
+        println!("{}","Out of range page number.".red());
+        return;
+    }
+
+    let total_items_in_table = app.get_total_number_of_items().unwrap_or(0);
+
+    if show_all {
+        println!("\n\tViewing All {} items\n",total_items_in_table.to_string().red())
+    } else {
+    let total_pages:f32 = (total_items_in_table as f32 / *limit as f32).ceil();
+        println!("\n\tPage {} of {} With {} Total items ({} items per page)\n",page.to_string().bright_green(),total_pages.to_string().yellow(),total_items_in_table.to_string().red(),limit.to_string().purple());
+    }
+
+    let mut table = Table::new(items);
+
+    table.with(Style::modern()).with(Colorization::exact([Color::FG_BLUE, Color::BOLD], Rows::first()));
+
+    println!("{table}");
+}
+
+fn set_item_status(id: &u32,status: &bool) {
+    let app = match Db::new() {
+        Ok(db) => db,
+        Err(_) =>  panic!("Can't connect to the database.")
+    };
+    let effected_rows =  app.set_item_status(id,status).unwrap_or(0);
+
+    if effected_rows == 0 {
+        eprint!("{}","No Item with that ID found.".red());
+        return;
+    }
+
+    println!("Item with id of {} got set as {}",id.to_string().bright_blue(),if *status {"Done".green()} else {"NotDone".red()});
+}
+
+fn delete_item(id: &u32) {
+    let app = match Db::new() {
+        Ok(db) => db,
+        Err(_) =>  panic!("Can't connect to the database.")
+    };
+    let effected_rows =  app.delete_item(id).unwrap_or(0);
+
+    if effected_rows == 0 {
+        eprint!("{}","No Item with that ID found.".red());
+        return;
+    }
+
+    println!("Item With id of {} got {}",id.to_string().bright_blue(),"DELETED".red().bold());
+}
+
+fn edit_item(id: &u32,name: &str, description: &str) {
+    let app = match Db::new() {
+        Ok(db) => db,
+        Err(_) =>  panic!("Can't connect to the database.")
+    };
+    let effected_rows =  app.edit_item(id,name,description).unwrap_or(0);
+
+    if effected_rows == 0 {
+        eprint!("{}","No Item with that ID found.".red());
+        return;
+    }
+
+    println!("Item With id of {} got {}",id.to_string().bright_blue(),"Edited".bright_purple());
+}
---
+++ src/repository/database.rs
@@ -0,0 +1,134 @@
+use colored::Colorize;
+use rusqlite::{named_params, params, Connection, Result};
+use tabled::Tabled;
+use tabled;
+
+#[derive(Tabled)]
+#[allow(non_snake_case)]
+pub struct TodoItem {
+    pub ID: i32,
+    pub Name: String,
+    pub Description: String,
+    #[tabled(display = "format_status")]
+    pub Status: bool,
+    pub CreatedAt: String,
+}
+
+
+fn format_status(status: &bool) -> String {
+    if *status {
+        "Done".green().to_string()
+    } else {
+        "NotDone".red().to_string()
+    }
+}
+
+pub struct Db {
+    conn: Connection,
+}
+
+impl Db {
+   pub fn new() -> Result<Self, rusqlite::Error> {
+        let conn = Connection::open("dolist.db")?;
+         conn.execute(
+        "CREATE TABLE IF NOT EXISTS dolist (\
+        id INTEGER PRIMARY KEY AUTOINCREMENT,\
+        name TEXT NOT NULL,\
+        description TEXT NOT NULL,\
+        done INTEGER NOT NULL,\
+        created TEXT NOT NULL)",())?;
+
+      Ok(Db { conn })
+   }
+
+  pub fn add_item(&self,name: &str,description: &str) -> Result<(), rusqlite::Error> {
+        self.conn.execute("INSERT INTO dolist \
+        (name, description, done, created) \
+        VALUES (?, ?, 0, datetime('now'))", (name,description))?;
+
+        Ok(())
+  }
+
+  pub fn show_items(&self,page: &u32, limit: &u32) -> Result<Vec<TodoItem>,rusqlite::Error> {
+      let offset = (page - 1) * limit;
+     let mut stmt = self.conn.prepare("SELECT * FROM dolist ORDER BY id DESC Limit :limit OFFSET :offset")?;
+      let result = stmt.query_map(params![limit, offset], |row| {
+          Ok(TodoItem {
+              ID: row.get(0)?,
+              Name: row.get(1)?,
+              Description: row.get(2)?,
+              Status: row.get(3)?,
+              CreatedAt: row.get(4)?,
+          })
+      })?;
+
+      let mut items = vec![];
+      for item in result {
+          items.push(item?);
+      }
+
+      Ok(items)
+  }
+
+    pub fn show_all_items(&self) -> Result<Vec<TodoItem>,rusqlite::Error> {
+        let mut stmt = self.conn.prepare("SELECT * FROM dolist ORDER BY id DESC")?;
+        let result = stmt.query_map([], |row| {
+            Ok(TodoItem {
+                ID: row.get(0)?,
+                Name: row.get(1)?,
+                Description: row.get(2)?,
+                Status: row.get(3)?,
+                CreatedAt: row.get(4)?,
+            })
+        })?;
+
+        let mut items = vec![];
+        for item in result {
+            items.push(item?);
+        }
+
+        Ok(items)
+    }
+    
+    pub fn get_total_number_of_items(&self) -> Result<u32, rusqlite::Error> {
+        let mut stmt = self.conn.prepare("SELECT COUNT(*) FROM dolist")?;
+        let result = stmt.query_map([], |row| {
+            row.get(0)
+        })?;
+        let mut count:u32 = 0;
+        for item in result {
+             count = item.unwrap_or(0);
+
+        }
+        Ok(count)
+    }
+    
+    pub fn set_item_status(&self, id: &u32, status: &bool) -> Result<usize, rusqlite::Error> {
+       let effected = self.conn.execute("UPDATE dolist SET done = ? WHERE id=?",(if *status {1} else {0},id))?;
+        
+        Ok(effected)
+    }
+    
+    pub fn delete_item(&self, id: &u32) -> Result<usize, rusqlite::Error> {
+        let effected =  self.conn.execute("DELETE FROM dolist WHERE id=?",[id])?;
+        
+        Ok(effected)
+    }
+
+    pub fn edit_item(&self, id: &u32, name: &str, description: &str) -> Result<usize, rusqlite::Error> {
+     let effected = self.conn.execute("UPDATE dolist
+        SET
+            name = CASE
+                WHEN :name = '' THEN name
+                ELSE :name
+            END,
+            description = CASE
+                WHEN :description = '' THEN description
+                ELSE :description
+            END
+        WHERE id = :id",named_params! {":id": id,":name": name, ":description": description})?;
+
+        Ok(effected)
+    }
+}
+
